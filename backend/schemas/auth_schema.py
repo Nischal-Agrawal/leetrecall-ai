572bb94a459f4b052d7181c7c1f6276d63f8b140
Nischal-Agrawal <nischalagrawal94@gmail.com>
2026-07-03 00:44:35 +0530

    Auto‑commit 2026-07-02T19:14:35.067Z

--- backend/schemas/auth_schema.py
+++ backend/schemas/auth_schema.py
@@ -1,21 +1,21 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
 class SignupRequest(BaseModel):
     username: str
-    email: EmailStr
+    email: str
     password: str
 
 
 class LoginRequest(BaseModel):
-    email: EmailStr
+    email: str
     password: str
 
 
 class UserResponse(BaseModel):
     id: int
     username: str
-    email: EmailStr
+    email: str
 
     class Config:
         from_attributes = True
